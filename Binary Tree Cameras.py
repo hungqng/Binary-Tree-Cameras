6cc57c4f049fb76d1c0e64d3ac660e8cc61f1416
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-06-17 17:41:26 -0700

    Add files via upload

--- Binary Tree Cameras.py
+++ Binary Tree Cameras.py
@@ -32,3 +32,14 @@ class Solution:
             return self.output + 1
         return self.output
 
+        # Solution 2
+        self.output =0
+        def dfs(root):
+            if not root:
+                return 2 # camera, monitor
+            l, r = dfs(root.left), dfs(root.right)
+            if l == 0 or r == 0:
+                self.output += 1
+                return 1
+            return 2 if l == 1 or r == 1 else 0
+        return (dfs(root) == 0) + self.output
